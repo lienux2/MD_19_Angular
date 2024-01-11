10935e8cf56a98a15c2ade630a7e16ea70a4e398
lienux2 <lienesvilpe5@gmail.com>
2024-01-11 17:48:52 +0200

    Second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { DinosaursComponent } from './components/dinosaurs/dinosaurs.component';
 import { FormsModule } from '@angular/forms';
@@ -15,7 +15,7 @@ import { AboutComponent } from './components/about/about.component';
     RouterModule,
     HeaderComponent,
     DinosaursComponent,
-    AboutComponent,
+    AboutComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
